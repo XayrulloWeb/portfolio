6ab645fc3a33d91fb9c79c95e611777c68c2fd1f
XayrulloWeb <xayrulloweb@gmail.com>
2025-02-11 22:25:39 +0300

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,178 @@
+import filmzone from "./assets/images/filmzone.jpg"
+import call from "./assets/images/call.jpg"
+import komparativistika from "./assets/images/komparativistika.jpg"
+import ubtuit from "./assets/images/ubtuit.jpg"
+import tahlil from "./assets/images/tahlil.jpg"
+
+
+
+const logotext = "XayrulloWeb";
+const meta = {
+    title: "Xayrullo",
+    description: "I’m Xayrullo data scientist _ Front-End devloper,currently working in Urgench",
+};
+
+const introdata = {
+    title: "I’m Xayrullo ",
+    animated: {
+        first: "I am lazy",
+        second: "I love coding",
+        third: "I develop Websites",
+    },
+    description: "Raxmonberganov Xayrullo is a front-end developer who creates user-friendly and aesthetic interfaces. Attentive to details, responsible, purposeful and strives for an ideal result.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Briefly about my self",
+    aboutme: "Raxmonberganov Xayrullo is a front-end developer who creates user-friendly and aesthetic interfaces. Attentive to details, responsible, purposeful and strives for an ideal result.",
+};
+const worktimeline = [{
+    jobtitle: "Front-End developer",
+    where: "Ubtuit",
+    date: "2024",
+},
+{
+    jobtitle: "Front-End developer",
+    where: "Amusoft",
+    date: "2025",
+},
+// {
+//     jobtitle: "Designer of week",
+//     where: "ALquds",
+//     date: "2019",
+// },
+];
+
+const skills = [{
+    name: "Html",
+    value: 90,
+},
+{
+    name: "Css",
+    value: 75,
+},
+{
+    name: "Javascript",
+    value: 40,
+},
+{
+    name: "React",
+    value: 70,
+},
+{
+    name: "Jquery",
+    value: 45,
+},
+{
+    name: "Gsap",
+    value: 35,
+},
+];
+
+const services = [{
+    title: "Web development ",
+    description: "Creation of modern and user-friendly websites with thoughtful UX/UI   ",
+},
+{
+    title: "Optimization and improvement",
+    description: "Improved performance, redesign and introduction of new features.",
+},
+{
+    title: "Mobile development(Test)",
+    description: "Development of cross-platform applications at Expo.",
+},
+];
+
+const dataportfolio = [{
+    img: filmzone,
+    description: "FilmZone is my own movie site where users can search for movies, view information about them, and add them to their watch list.",
+    link: "https://filmzonee.netlify.app/",
+},
+{
+    img: call,
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "http://tourismservice.uz/",
+},
+{
+    img: komparativistika,
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://komparativistika.uz/",
+},
+{
+    img: ubtuit,
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://www.ubtuit.uz/",
+},
+{
+    img: tahlil,
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://rahimbergan.netlify.app/",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "xayrulloweb@gmail.com",
+    YOUR_FONE: "+998(99)-564-02-01",
+    description: "Ready for cooperation and new projects! Contact me in a convenient way:",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_wis39e1",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/XayrulloWeb",
+    instagram: "https://www.instagram.com/xayrullo01_02/",
+    linkedin: "https://www.linkedin.com/in/xayrullo-raxmonberganov-89bbab343/",
+    telegram: "https://t.me/Xayrullo01_02",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
